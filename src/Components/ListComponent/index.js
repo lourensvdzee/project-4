f1a9b04da65207724eba42f396032852a85d3179
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-19 14:51:48 +0200

    deleted bulletpoints

--- src/Components/ListComponent/index.js
+++ src/Components/ListComponent/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './ListComponent.css';
 
 function ListComponent({ activities, isGoodWeather, onDeleteActivity }) {
     return (
         <>
             <h2>{isGoodWeather ? "The weather is awesome! Go outside and:" : "Bad weather outside! Here's what you can do now:"}</h2>
-            <ul>
+            <ul className="activity-list">
                 {activities.map(activity => (
                     <li key={activity.id}>
                         {activity.isForGoodWeather ? '🌞 ' : '🌧 '} {activity.name}
