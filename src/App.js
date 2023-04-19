03c2f1b60d2b26e0859d88eab0dae73938f8dcf3
Lourens-Spiced <lourensvdzee@gmail.com>
2023-04-19 12:08:29 +0200

    fetch + heading finished

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import ListComponent from './Components/ListComponent';
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", { defaultValue: [] });
   const [weather, setWeather] = useState(null);
+  const [condition, setCondition] = useState('');
+  const [temperature, setTemperature] = useState('');
 
   useEffect(() => {
     localStorage.setItem('activities', JSON.stringify(activities));
@@ -17,6 +19,8 @@ function App() {
         const response = await fetch("https://example-apis.vercel.app/api/weather");
         const data = await response.json();
         setWeather(data.isGoodWeather);
+        setCondition(data.condition);
+        setTemperature(data.temperature);
       } catch (error) {
         console.log(error);
       }
@@ -32,9 +36,11 @@ function App() {
 
   return (
     <div>
+      <h1>{condition} {temperature} °C</h1>
       <Form onAddActivity={handleAddActivity} />
       <ListComponent activities={filteredActivities} isGoodWeather={weather} />
     </div>
   );
 }
+
 export default App;
